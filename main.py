fbaa24cbbd1124795e0f4026bfcff37ed1251432
zhitingli12 <zhitingli54678@gmail.com>
2024-04-11 14:19:16 -0400

    Menu

---
+++ main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        encode_password = int(input("Please enter your password to encode: "))
+        print("Your password has been encoded and stored!")
+    elif option == 2:
+
+    elif option == 3:
+        break
+
+
